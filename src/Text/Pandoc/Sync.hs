826169bf66488eb60fb93663f4ea04b0f0106a7e
Justin Le <justin@jle.im>
2018-03-15 15:02:09 -0700

    got merging and syncing purely part?

--- src/Text/Pandoc/Sync.hs
+++ src/Text/Pandoc/Sync.hs
@@ -1,44 +1,60 @@
-{-# LANGUAGE GADTs             #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TypeInType        #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeInType          #-}
 
 module Text.Pandoc.Sync (
     Config
-  , classify
+  , updateCanonical
+  , syncBranches
+  , updateBranch
   ) where
 
 -- import           Control.Alternative.Free.Final
+-- import           Control.Applicative.Free
+-- import           Data.List
+-- import qualified Data.Aeson.Types               as A
+-- import qualified Data.ByteString                as BS
+-- import qualified Data.Text                      as T
 import           Control.Lens
 import           Control.Monad.Operational
 import           Data.Aeson
 import           Data.Default
+import           Data.Diff
+import           Data.Foldable
 import           Data.Kind
-import           Data.List
 import           Data.Maybe
-import           Data.Semigroup
+import           Data.Semigroup hiding             (diff)
 import           Data.Time
 import           System.FilePath
 import           Text.Pandoc.Sync.Format
-import qualified Data.Aeson.Types                  as A
-import qualified Data.ByteString                   as BS
+import qualified Data.List.NonEmpty                as NE
 import qualified Data.Map                          as M
 import qualified Data.Map.Merge.Lazy               as M
 import qualified Data.Set                          as S
-import qualified Data.Text                         as T
+import qualified Generics.SOP                      as SOP
 import qualified Text.Pandoc.Definition            as P
 
 type CanPath = FilePath
+type Extension = String
 
 data BranchSpec = BS { _bsRoot   :: FilePath
                      , _bsFormat :: SomeFormat
+                     , _bsExt    :: Extension
                      }
   deriving (Show, Eq, Ord)
+makeLenses ''BranchSpec
 
 data CFile = CF { _cfPandoc :: P.Pandoc
-                , _cfUpdate :: (UTCTime, BranchSpec)
+                , _cfUpdate :: UTCTime
                 }
   deriving (Show, Eq, Ord)
+
+makeLenses ''CFile
 
 -- data BFile = BF { _bfCFile  :: FilePath         -- ^ canonical file?
 --                 , _bfUpdate :: UTCTime
@@ -90,7 +106,7 @@ instance FromJSON FileConfig where
     parseJSON = withObject "FileConfig" $ \v -> do
       opts     <- v .:? "options"  .!= def
       branches <- v .:? "branches" .!= M.empty
-      return $ FC opts (collapseMap BS branches)
+      return $ FC opts (collapseMap (\p (f, e) -> BS p f e) branches)
 
 instance FromJSON Opts where
     parseJSON = withObject "Opts" $ \v ->
@@ -101,27 +117,29 @@ instance FromJSON Config where
         opts     <- v .:? "options"  .!= def
         branches <- v .:? "branches" .!= M.empty
         files    <- v .:? "files"    .!= M.empty
-        return $ C opts (collapseMap BS branches) files
+        return $ C opts (collapseMap (\p (f, e) -> BS p f e) branches) files
 
-classify
-    :: Config
-    -> FilePath
-    -> M.Map BranchSpec CanPath     -- canonical
-classify C{..} fp = M.fromList $ do
-    rootElems <- drop 1 . reverse . inits . splitPath $ fp
-    let root = takeDirectory . joinPath $ rootElems
-        bs   = BS root format
-        can  = dropExtension . makeRelative root $ fp
-    _ <- maybeToList $ M.lookup bs _cBranches
-    return (bs, can)
-  where
-    format = inferFormat (drop 1 (takeExtension fp))
+-- classify
+--     :: Config
+--     -> FilePath
+--     -> M.Map BranchSpec CanPath     -- canonical
+-- classify C{..} fp = M.fromList $ do
+--     rootElems <- drop 1 . reverse . inits . splitPath $ fp
+--     let root = takeDirectory . joinPath $ rootElems
+--         bs   = BS root format
+--         can  = dropExtension . makeRelative root $ fp
+--     _ <- maybeToList $ M.lookup bs _cBranches
+--     return (bs, can)
+--   where
+--     format = inferFormat (drop 1 (takeExtension fp))
 
--- data CatF :: Type -> Type where
---     CatFile :: FilePath -> CatF (Maybe T.Text)
+data LoadPF :: Type -> Type where
+    LoadP :: FilePath -> LoadPF P.Pandoc
 
--- type Cat = Alt CatF
+type LoadP = Program LoadPF
 
+loadP :: FilePath -> LoadP P.Pandoc
+loadP = singleton . LoadP
 
 -- Data flow:
 --
@@ -142,6 +160,12 @@ classify C{..} fp = M.fromList $ do
 --                      , _fuBody :: BS.ByteString
 --                      }
 
+realizedPath
+    :: BranchSpec
+    -> CanPath
+    -> FilePath
+realizedPath BS{..} p = _bsRoot </> p -<.> _bsExt
+
 updateBranch
     :: CanPath
     -> UTCTime
@@ -152,45 +176,56 @@ updateBranch p t = set (at p) (Just t)
 updateCanonical
     :: M.Map BranchSpec Branch
     -> Canonical
-    -> Canonical
-updateCanonical bs = M.merge newPath noUpdate updated paths
+    -> LoadP Canonical
+updateCanonical bs = M.mergeA (M.traverseMaybeMissing $ \p -> mkUpdate p mempty)
+                              M.preserveMissing
+                              (M.zipWithMaybeAMatched $ \p us (CF pd0 t0) ->
+                                    let (_, candidates) = M.split t0 us
+                                    in  mkUpdate p pd0 candidates
+                              )
+                              paths
   where
+    -- | Map of all canonical paths inside 'bs', sorted by update time
     paths :: M.Map CanPath (M.Map UTCTime (S.Set BranchSpec))
     paths = (M.unionsWith . M.unionWith) S.union
           . ifoldMap (\sp -> (:[]) . fmap (flip M.singleton (S.singleton sp)))
           $ bs
-    newPath :: M.SimpleWhenMissing CanPath (M.Map UTCTime (S.Set BranchSpec)) CFile
-    newPath = M.mapMaybeMissing $ \p us -> CF _ _
-    noUpdate :: M.SimpleWhenMissing CanPath CFile CFile
-    noUpdate = M.preserveMissing
-    updated :: M.SimpleWhenMatched CanPath (M.Map UTCTime (S.Set BranchSpec)) CFile CFile
-    updated = undefined
+    mkUpdate
+        :: CanPath
+        -> P.Pandoc
+        -> M.Map UTCTime (S.Set BranchSpec)
+        -> LoadP (Maybe CFile)
+    mkUpdate p pd0 us = do
+        res <- fmap (mergeDocs pd0)
+             . traverse (loadP . flip realizedPath p)
+             . foldMap toList
+             $ us
+        pure $ CF res . fst <$> M.lookupMax us
 
-    -- -> Branch
-    -- -> Canonical
-    -- -> Canonical
--- updateCanonical
--- data SyncState = SS { _ssCanonical :: Canonical
-    --                 , _ssBranches  :: M.Map BranchSpec Branch
-    --                 }
-
-
--- type Canonical = M.Map CanPath CFile
+syncBranches
+    :: Canonical
+    -> M.Map BranchSpec Branch
+    -> M.Map BranchSpec Branch
+syncBranches cs = fmap go
+  where
+    go :: Branch -> Branch
+    go = M.merge (M.mapMissing $ \_ (CF _ t) -> t)
+                 M.dropMissing
+                 (M.zipWithMatched $ \_ (CF _ t0) t1 -> max t0 t1)
+                 cs
 
 -- type Branch    = M.Map CanPath UTCTime
 
--- data SyncState = SS { _ssCanonical :: Canonical
---                     , _ssBranches  :: M.Map BranchSpec Branch
---                     }
-
--- data FileUpdate = FU { _fuUpdate   :: UTCTime
---                      , _fuContents :: BS.ByteString
---                      }
-
--- data UpdateType = UTChange
---                 | UTAdd
-
-
+mergeDocs :: P.Pandoc -> [P.Pandoc] -> P.Pandoc
+mergeDocs orig ps = fromMaybe orig $ do
+    e NE.:| es <- fmap (diff orig) <$> NE.nonEmpty ps
+    patch (foldl' go e es) orig
+  where
+    go :: Edit P.Pandoc -> Edit P.Pandoc -> Edit P.Pandoc
+    go old new = case mergePatch old new of
+      Incompatible -> old
+      Conflict p   -> p
+      NoConflict p -> p
 
 -- updateSync
 --     :: FileUpdate
@@ -201,3 +236,44 @@ updateCanonical bs = M.merge newPath noUpdate updated paths
 -- data Update = FileChange FilePath UTCTime BS.ByteString
 --             | FileAdd    FilePath UTCTime BS.ByteString
 
+instance SOP.Generic P.Pandoc
+instance SOP.Generic P.Meta
+instance SOP.Generic P.MetaValue
+instance SOP.Generic P.Block
+instance SOP.Generic P.Inline
+instance SOP.Generic P.Format
+instance SOP.Generic P.QuoteType
+instance SOP.Generic P.ListNumberStyle
+instance SOP.Generic P.ListNumberDelim
+instance SOP.Generic P.Alignment
+instance SOP.Generic P.MathType
+instance SOP.Generic P.Citation
+instance SOP.Generic P.CitationMode
+
+instance SOP.HasDatatypeInfo P.Pandoc
+instance SOP.HasDatatypeInfo P.Meta
+instance SOP.HasDatatypeInfo P.MetaValue
+instance SOP.HasDatatypeInfo P.Block
+instance SOP.HasDatatypeInfo P.Inline
+instance SOP.HasDatatypeInfo P.Format
+instance SOP.HasDatatypeInfo P.QuoteType
+instance SOP.HasDatatypeInfo P.ListNumberStyle
+instance SOP.HasDatatypeInfo P.ListNumberDelim
+instance SOP.HasDatatypeInfo P.Alignment
+instance SOP.HasDatatypeInfo P.MathType
+instance SOP.HasDatatypeInfo P.Citation
+instance SOP.HasDatatypeInfo P.CitationMode
+
+instance Diff P.Pandoc
+instance Diff P.Meta
+instance Diff P.MetaValue
+instance Diff P.Block
+instance Diff P.Inline
+instance Diff P.Format
+instance Diff P.QuoteType
+instance Diff P.ListNumberStyle
+instance Diff P.ListNumberDelim
+instance Diff P.Alignment
+instance Diff P.MathType
+instance Diff P.Citation
+instance Diff P.CitationMode
